87a4a307e727438e42e452db9f58bf6c251f2e01
IgLemp <iglemp@protonmail.com>
2023-12-16 23:58:03 +0100

    initial structure

--- main.c
+++ main.c
@@ -2,10 +2,13 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define RANGE 200
 #define HEIGHT 50
 
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
+#define MIN(a, b) ((a) < (b) ? (a) : (b))
 
 typedef struct {
     int year, month, day;
@@ -18,23 +21,28 @@ typedef struct {
     int volume;
 } Flare;
 
+// When I look at error handling in this file I start to miss monads
+
 int main(int argc,char *argv[]) {
-    if (argc < 2) { puts("No file provided."); exit(1); }
+    if (argc < 2) { puts("No input or output file provided."); exit(1); }
     
-    FILE *file_fd = fopen(argv[1], "r");
-    if (!file_fd) { puts("Incorrect file name provided."); exit(1); }
+    FILE *input_fd = fopen(argv[1], "r");
+    if (!input_fd) { puts("Incorrect input file name provided."); exit(1); }
+
+    FILE *output_fd = fopen(argv[2], "w");
+    if (!input_fd) { puts("Incorrect output file name provided."); exit(1); }
 
     // Find size of file in number of chars
-    fseek(file_fd, 0, SEEK_END);
-    int buffer_s = ftell(file_fd);
-    rewind(file_fd);
+    fseek(input_fd, 0, SEEK_END);
+    int buffer_s = ftell(input_fd);
+    rewind(input_fd);
 
     char *buffer = (char*)malloc(sizeof(char) * buffer_s);
     if (!buffer) { puts("Memory allocation error!"); exit(1); }
 
-    int ok = fread(buffer, 1, buffer_s, file_fd);
+    int ok = fread(buffer, 1, buffer_s, input_fd);
     if (!ok) { puts("Error while reading file."); exit(1); }
-    fclose(file_fd);
+    fclose(input_fd);
 
     char *token;
     token = strtok(buffer, ",\n");
@@ -52,30 +60,90 @@ int main(int argc,char *argv[]) {
 
     for (int i = 0; i < data_s - 1; i++) {
         char* line = strtok(NULL, "\n");
-        // Parse tokens
+        // Parse lines
         int year, month, day;
         double open, close;
         double high, low;
         double volume;
 
-        sscanf(line, "%4d-%2d-%2d,%lf,%lf,%lf,%lf,%lf", &year, &month, &day, &open, &close, &high, &low, &volume);
+        sscanf(line, "%4d-%2d-%2d,%lf,%lf,%lf,%lf,%lf", &year, &month, &day, &open, &high, &low, &close, &volume);
         data[i] = (Flare){{year, month, day}, open, close, high, low, volume};
 
-        // printf("[DEBUG] %4d-%2d-%2d: %lf %lf %lf %lf %lf\n", year, month, day, open, close, high, low, volume);
+        // printf("[DEBUG] %04d-%02d-%02d: %lf %lf %lf %lf %lf\n", year, month, day, open, high, low, close, volume);
     }
 
-    for (int i = 0; i < data_s - 1; i++) {
-        int year  = data[i].date.year;
-        int month = data[i].date.month;
-        int day   = data[i].date.day;
-        double open   = data[i].open;
-        double close  = data[i].close;
-        double high   = data[i].high;
-        double low    = data[i].low;
-        double volume = data[i].volume;
+    // allocate array for strings
+    char **graph = (char**)malloc(sizeof(char*) * RANGE);
+    if (!graph) { puts("Memory allocation error!"); exit(1); }
 
-        printf("[DEBUG] %4d-%02d-%02d: %lf %lf %lf %lf %lf\n", year, month, day, open, close, high, low, volume);
+    // allocate said strings, remember null terminator because C
+    for (int i; i < RANGE; i++) {
+        graph[i] = (char*)malloc(sizeof(char) * (HEIGHT + 1));
+        if (!graph[i]) { puts("Memory allocation error!"); exit(1); }
     }
+
+    // currently temporary restriction
+    if (data_s < RANGE) { puts("Minimum number of records is 200"); exit(1); }
+
+    // Find highest and lowest value in range
+    double highest = MAX(data[0].high, data[0].low);
+    double lowest  = MIN(data[0].high, data[0].low);
+    for (int i = 0; i < RANGE; i++) {
+        if (highest < data[i].high) { highest = data[i].high; }
+        if (highest < data[i].low ) { highest = data[i].low ; }
+        if (lowest  > data[i].high) { lowest  = data[i].high; }
+        if (lowest  > data[i].low ) { lowest  = data[i].low ; }
+    }
+    double amplitude = highest - lowest;
+    double scale_factor = (HEIGHT / amplitude);
+
+    for (int i = 0; i < RANGE; i++) {
+        Flare flare = data[i];
+        bool falling = flare.open > flare.close;
+        double top = (flare.high - lowest) * scale_factor;
+        double bot = (flare.low  - lowest) * scale_factor;
+        double floor, ceil;
+
+        if (falling) { ceil = (flare.open  - lowest) * scale_factor; floor = (flare.close - lowest) * scale_factor; }
+        else         { ceil = (flare.close - lowest) * scale_factor; floor = (flare.open  - lowest) * scale_factor; }
+
+        // Set string
+        memset(graph[i], ' ', HEIGHT);
+        graph[i][HEIGHT] = '\0';
+        
+        // Fill string
+        for (int j = 0; j < HEIGHT; j++) {
+            if (j > bot && j < floor)  { graph[i][j] = '|'; }
+            if (j > floor && j < ceil) { graph[i][j] = (falling ? 'O' : '#'); }
+            if (j > ceil && j < top)   { graph[i][j] = '|'; }
+        }
+
+        // printf("[DEBUG] %4d-%02d-%02d:            open: %lf   close: %lf   high: %lf   low:   %lf\n", flare.date.year, flare.date.month, flare.date.day, flare.open, flare.close, flare.high, flare.low);
+        // printf("[DEBUG] %4d-%02d-%02d: fall: %d    top:  %lf    bot:   %lf    ceil: %lf    floor: %lf\n", flare.date.year, flare.date.month, flare.date.day, falling, top, bot, ceil, floor);
+    }
+
+    for (int i = 0; i < HEIGHT; i++) {
+        for (int j = 0; j < RANGE; j++) {
+            fprintf(output_fd, "%c", graph[j][i]);
+        }
+        fprintf(output_fd, "\n");
+    }
+
+    // printf("highest: %lf    lowest: %lf\n", highest, lowest);
+    // printf("scale factor: %lf\n", scale_factor);
+
+    // for (int i = 0; i < data_s - 1; i++) {
+    //     int year  = data[i].date.year;
+    //     int month = data[i].date.month;
+    //     int day   = data[i].date.day;
+    //     double open   = data[i].open;
+    //     double close  = data[i].close;
+    //     double high   = data[i].high;
+    //     double low    = data[i].low;
+    //     double volume = data[i].volume;
+
+    //     printf("[DEBUG] %4d-%02d-%02d: %lf %lf %lf %lf %lf\n", year, month, day, open, close, high, low, volume);
+    // }
 
     return 0;
 }
